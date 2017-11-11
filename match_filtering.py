2340c91a2fa567d676e63eb5d9401c3b1988c261
James Lucas <jameslucas@jamess-mbp.attlocal.net>
2017-11-10 23:42:45 -0800

    Added try/except block... some of the scafolds have Rosetta scores appended and prody is unable to parse them... apparently...

--- match_filtering.py
+++ match_filtering.py
@@ -383,20 +383,27 @@ if __name__ == '__main__':
             :param matched_PDB: path to a single matched PDB
             """
             match_name = os.path.basename(os.path.normpath(matched_PDB))
-            match_prody = prody.parsePDB(matched_PDB)
+            
+            row_dict = {'match_name': match_name,
+                        'ligand_shell_eleven': 0,
+                        'interface_CB_contact_percentage': 0,
+                        'motif_shell_CB': 0,
+                        'residue_match_score': 9999,
+                        'ligand_match_score': 9999,
+                        'min_res_per_chain': 0,
+                        'gurobi_motif_score': 0,
+                        'ligand_CB_clashes': 9999
+                        }
 
-            if any(match_prody.select('name CB within 11 of resname {}'.format(ligand)) == None, match_prody.select('protein') ==None):
-                row_dict = {'match_name': match_name,
-                            'ligand_shell_eleven': 0,
-                            'interface_CB_contact_percentage': 0,
-                            'motif_shell_CB': 0,
-                            'residue_match_score': 9999,
-                            'ligand_match_score': 9999,
-                            'min_res_per_chain': 0,
-                            'gurobi_motif_score': 0,
-                            'ligand_CB_clashes': 9999
-                            }
+            try:
+                match_prody = prody.parsePDB(matched_PDB)
+            except:
+                match_prody = False
 
+            if not match_prody:
+                return row_dict
+
+            if any([match_prody.select('name CB within 11 of resname {}'.format(ligand)) == None, match_prody.select('protein') ==None]):
                 return row_dict
 
             # Parse matched_PDB to get ideal binding site name and residues
